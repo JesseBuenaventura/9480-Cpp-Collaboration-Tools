cb2a36d8bb81b76531747f82717084487f0f5ae4
JESSE DANIEL BUENAVENTURA <2221096@slu.edu.ph>
2023-09-15 10:26:34 +0800

    added reserving of slot functionality

--- Slot.cpp
+++ Slot.cpp
@@ -26,10 +26,55 @@ void viewAvailableSlots(const vector<Slot>& slots){
 
     for (const Slot& slot : slots) {
         if (slot.isAvailable) {
-            cout << "Slot " << slot.slotNumber << " is available." << endl;
+            cout << "Slot " << slot.slotNumber << "\tis available." << endl;
         } else {
-            cout << "Slot " << slot.slotNumber << " is occupied." << endl;
+            cout << "Slot " << slot.slotNumber << "\tis occupied." << endl;
         }
+    }
+}
+
+void reserveSlot(vector<Slot>& slots){
+    cout << "Enter Slot:" << endl;
+    int slot;
+    cin >> slot;
+
+    // Checks if slot is already occupied
+    if(!slots[slot - 1].isAvailable){
+        cout << "Slot " << slot << " is already occupied. Please choose another slot." << endl;
+    } else {
+
+        // TO DO: Check if student id is valid 
+        cout << "School ID Number:" << endl;
+        int idNumber;
+        cin >> idNumber;
+        cin.ignore();
+
+        cout << "Student Name:" << endl;
+        string studentName;
+        getline(cin,studentName); //getline() reads a string from input
+
+        //TO DO: Decide if number of hours nalang or Arrival Time nalang
+        //TO DO: Handle wrong inputs
+        cout << "Enter the Number of Hours:" << endl;
+        int hours;
+        cin >> hours;
+        cin.ignore();
+
+        cout << "Departure Time:" << endl;
+        string departure;
+        getline(cin, departure); //getline() reads a string from input
+
+        slots[slot - 1].isAvailable = false; //slot # availability will become false after booking a slot
+
+        //displays reservation info
+        cout << "-----------------------------" << endl;
+        cout << "Confirm Reservation" << endl;
+        cout << "Details:" << endl;
+        cout << "Slot Number: " << slot << endl;
+        cout << "ID Number: " << idNumber << endl;
+        cout << "Name: " << studentName << endl;
+        cout << "Time: " << departure << endl;
+        cout << "-----------------------------" << endl;
     }
 }
 
@@ -38,7 +83,7 @@ vector<Slot> slots = initializeSlots(10);
 int choice;
 
     while(true) {
-        cout << "----Choose an option----" << endl;
+        cout << "\n----Choose an option----" << endl;
         cout << "1. View Available Slots" << endl;
         cout << "2. View my Reservations" << endl;
         cout << "3. Cancel Reservation" << endl;
@@ -49,8 +94,29 @@ int choice;
         switch (choice)
         {
         case 1:
+            cout << "View Available Slots" << endl;
             viewAvailableSlots(slots);
-            break;
+
+            char reserveChoice;
+            do{
+                cout << "Do you want to reserve a slot? (Input Y/y for Yes, N/n for No): ";
+                cin >> reserveChoice;
+            
+            switch(reserveChoice){
+            case 'y':
+            case 'Y':
+                reserveSlot(slots);
+                break;
+            case 'n':
+            case 'N':
+                break;
+            default:
+                cout << "Invalid choice." << endl;
+                break;
+            }
+        } while (reserveChoice != 'n' && reserveChoice != 'N');
+        break;
+
         case 2:
             cout << "View my Reservations" << endl;
             break;
